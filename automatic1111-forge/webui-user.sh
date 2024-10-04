49fcd48a2db3eacbc1d01f34bf1f8545620a7b9b
Nathan <NathanJKWilson@outlook.com>
2024-10-04 10:52:08 +0100

    Updated command line for xformers

--- automatic1111-forge/webui-user.sh
+++ automatic1111-forge/webui-user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export COMMANDLINE_ARGS="$COMMANDLINE_ARGS --enable-insecure-extension-access --listen"
-
 python_cmd="python3.10"
+export COMMANDLINE_ARGS="$COMMANDLINE_ARGS --enable-insecure-extension-access --listen --xformers"
+export LD_PRELOAD=libtcmalloc.so
